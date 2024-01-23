72ff17c21038adcd96d2d6c80d28cb6f2f48386a
Corey Donahue <cdartswebdev@gmail.com>
2024-01-23 10:29:26 -0700

    added /index to the get requests

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
-app.get("/", (req, res) => {
+app.get("/index", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
